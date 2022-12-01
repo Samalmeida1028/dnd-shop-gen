07f3c353e2538cc934b7de5762a74cd99c89f1f6
Samalmeida1028 <80793329+Samalmeida1028@users.noreply.github.com>
2022-11-30 22:05:02 -0500

    Update Item.py

--- Objects/Item.py
+++ Objects/Item.py
@@ -1,9 +1,10 @@
 import json
 
+
 class Item:
 
-    def __init__(self, name,  baseRegion, itemType = "misc", baseValue = "1", rarity = "1", arg = []):
-        if(len(arg) == 0):
+    def __init__(self, name, baseRegion, itemType="misc", baseValue="1", rarity="1", arg=[]):
+        if len(arg) == 0:
             self.name = name
             self.itemType = itemType
             self.baseValue = baseValue
@@ -16,16 +17,17 @@ class Item:
             self.rarity = arg[3]
             self.baseRegion = arg[4]
 
-
-
     def __str__(self):
-        return '{"Name":"' + self.name + '","Type":"' + self.itemType + '","Value":"' + self.baseValue + '","Rarity":"' + self.rarity + '","Base Region":"' + self.baseRegion + '"}'
+        return '{"Name":"' + self.name + '","Type":"' + self.itemType + '","Value":"' + self.baseValue + '","Rarity":"'\
+               + self.rarity + '","Base Region":"' + self.baseRegion + '"}'
+
     def str(self):
         return self.__str__()
 
+
 class ItemManager:
 
-    def __init__(self): #inits by trying to load a items.txt json for the save data, else it creates a new one
+    def __init__(self):  # inits by trying to load an items.txt json for the save data, else it creates a new one
         temp = ""
         self.itemTypes = []
         self.itemRegions = []
@@ -36,41 +38,43 @@ class ItemManager:
                 file.close()
         except FileExistsError:
             try:
-                with open("../Resources/save_data/items.txt") as file: # loads the json file into a dictionary
+                with open("../Resources/save_data/items.txt") as file:  # loads the json file into a dictionary
                     self.itemList = json.load(file)
             except json.decoder.JSONDecodeError:
                 self.itemList = {}
 
-    def addItem(self, name, itemType, baseValue, rarity, baseRegion): #add item by property
+    def addItem(self, name, itemType, baseValue, rarity, baseRegion):  # add item by property
         temp = Item(name, itemType, baseValue, rarity, baseRegion)
         self.itemList[name] = json.loads(temp.str())
 
-    def addItem(self, arrlist): ##add new item by list
-        temp = Item("","","","","",arrlist)
-        self.itemList[arrlist[0]] = json.loads(temp.str())
+    def addItem(self, item_list):  # add new item by list
+        temp = Item("", "", "", "", "", item_list)
+        self.itemList[item_list[0]] = json.loads(temp.str())
 
-    def getItemRegions(self): #returns a list of all the regions in the item list
+    def getItemRegions(self) -> list:  # returns a list of all the regions in the item list
         for key in self.itemList:
             if self.itemList[key]["Base Region"] not in self.itemRegions:
                 self.itemRegions.append(self.itemList[key]["Base Region"])
         return self.itemRegions
 
-    def getItemTypes(self): #gets all the item types in the item list
+    def getItemTypes(self) -> list:  # gets all the item types in the item list
         for key in self.itemList:
             if self.itemList[key]["Type"] not in self.itemTypes:
                 self.itemTypes.append(self.itemList[key]["Type"])
         return self.itemTypes
 
-    def getItemIndex(self, index): #gets the index of an item in the item list
-        itemName = list(self.itemList)[int(index)]
-        item = Item(itemName, self.itemList[itemName]["Base Region"], self.itemList[itemName]["Type"], self.itemList[itemName]["Value"], self.itemList[itemName]["Rarity"])
-        return item;
-
-    def getItemName(self,name): #gets an item if given a name
-        item = Item(name, self.itemList[name]["Base Region"], self.itemList[name]["Type"], self.itemList[name]["Value"], self.itemList[name]["Rarity"])
+    def getItemIndex(self, index) -> Item:  # gets the index of an item in the item list
+        item_name = list(self.itemList)[int(index)]
+        item = Item(item_name, self.itemList[item_name]["Base Region"], self.itemList[item_name]["Type"],
+                    self.itemList[item_name]["Value"], self.itemList[item_name]["Rarity"])
         return item
 
-    def saveItems(self): # saves the items to the txt file
+    def getItemName(self, name) -> Item:  # gets an item if given a name
+        item = Item(name, self.itemList[name]["Base Region"], self.itemList[name]["Type"], self.itemList[name]["Value"],
+                    self.itemList[name]["Rarity"])
+        return item
+
+    def saveItems(self):  # saves the items to the txt file
         open('../Resources/save_data/items.txt', 'w').close()
         file = open("../Resources/save_data/items.txt", "r+")
         json.dump(self.itemList, file, indent=4)
